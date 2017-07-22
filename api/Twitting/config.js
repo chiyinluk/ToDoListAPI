71c56e3e960dbfdc65748a53e2be12e993cdd425
chiyinluk <chiyinluk@gmail.com>
2017-07-21 23:37:56 -0400

    Add files
    Add files

---
+++ api/Twitting/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  consumer_key:         '8NGihN9rQblxSmNB6mdk9IYry',
+  consumer_secret:      'iaMVMxIWoSpIw98p7es9Z9fqZkoUqJG48MQ675Pc7tNErQNay5',
+  access_token:         '69793820-oLIMDUyaaYNT7hIHvtVrNyMzldbRSILqfObvevtIT',
+  access_token_secret:  'cNKLhPx7BUblQIJMHxYtF8BVUt7T4NInycn5nZAcBspks',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+
+}
